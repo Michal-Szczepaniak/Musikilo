db471c991035212fc256177ba88998fd8945616a
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-12-04 15:33:24 +0800

    Create harbour-Musikilo-zh_CN.ts

---
+++ Musikilo/translations/harbour-Musikilo-zh_CN.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak.</source>
+        <translation>由 Michał Szczepaniak 开发。</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation>采用 GPLv3 许可协议</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>链接</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>感谢</translation>
+    </message>
+    <message>
+        <source>jgibbon for help.</source>
+        <translation>感谢 jgibbon 的帮助。</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>捐赠</translation>
+    </message>
+</context>
+<context>
+    <name>FilesList</name>
+    <message>
+        <source>Files list</source>
+        <translation>文件列表</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Hostname</source>
+        <translation>主机名</translation>
+    </message>
+    <message>
+        <source>Root path</source>
+        <translation>Root 路径</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>用户名</translation>
+    </message>
+    <message>
+        <source>Port</source>
+        <translation>端口</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>连接</translation>
+    </message>
+</context>
+<context>
+    <name>SongDetails</name>
+    <message>
+        <source>Album: %1</source>
+        <translation>专辑: %1</translation>
+    </message>
+    <message>
+        <source>Year: %1</source>
+        <translation>年份: %1</translation>
+    </message>
+    <message>
+        <source>Track No.: %1</source>
+        <translation>音乐编号: %1</translation>
+    </message>
+    <message>
+        <source>Bit rate: %1 kb/s</source>
+        <translation>比特率:%1 kb/s</translation>
+    </message>
+    <message>
+        <source>Song %1/%2</source>
+        <translation>音乐 %1/%2</translation>
+    </message>
+</context>
+<context>
+    <name>SongsList</name>
+    <message>
+        <source>Playlist</source>
+        <translation>播放列表</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>移除</translation>
+    </message>
+</context>
+</TS>
